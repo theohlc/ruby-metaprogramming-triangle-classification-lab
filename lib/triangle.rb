f3282eca77e17133436cfa076ed8cddbfa0088a0
theo <teddyhoover@gmail.com>
2020-12-25 16:03:26 -0600

    commit tree 1629a3a8400b0afc8e432c585356ca63cfdbe368

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c 
+  
+  def initialize(side_a, side_b, side_c)
+    @a = side_a
+    @b = side_b
+    @c = side_c
+  end
+
+  def kind
+    if a + b <= c || b + c <= a || c + a <= b || a == 0 || b == 0 || c == 0
+      raise TriangleError
+    elsif a == b && b == c 
+      :equilateral
+    elsif a == b || b == c || c == a 
+      :isosceles
+    else  
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
